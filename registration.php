953aa9adb357f4e6ea44d29ff2303345bdb54d42
stheme <47854255+stheme@users.noreply.github.com>
2019-03-19 10:30:54 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sharwood/Wood',
+    'frontend/stheme/sharwood',
     __DIR__
 );
